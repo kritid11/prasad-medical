120753969ff446782b75336975f078823b7adcc4
Kriti Dudhiya <kritid11@gmail.com>
2018-04-06 21:01:19 +0530

    fb-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { NewRequestPage } from '../pages/new_request/new_request';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Facebook } from '@ionic-native/facebook';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    NewRequestPage
   ],
   imports: [
     BrowserModule,
@@ -23,12 +29,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    NewRequestPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    [Facebook]
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+import { ListPage } from '../list/list';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	  afterSplash: boolean = false; 
 
-  }
+	  constructor(public navCtrl: NavController) {
+
+	  	let TIME_IN_MS = 2000;
+		let hideFooterTimeout = setTimeout( () => {
+		     // somecode
+		     this.afterSplash=true;
+		}, TIME_IN_MS);
+	  }
+
+	  goToLogin(){
+	    this.navCtrl.push(LoginPage);
+	  }
+
+	  goToSignUp(){
+	  	this.navCtrl.push(ListPage);
+	  }
 
 }
